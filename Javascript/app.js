9cafb09f28271cb45afeda04c8535cdaefa29190
Manoj-Katkar <education2001manojkatkar@gmail.com>
2024-05-27 15:46:58 +0530

    started one new static project and also learned the javascript Engine how it works in the background

---
+++ Javascript/app.js
@@ -0,0 +1,46 @@
+// ^here it will get display in the console
+// console.log("Hello Js I am learning to you that How to Implement");
+// console.log('KKR are won the IPL');
+// console.log('hello thala');
+
+// document.write("MK : Hey Hi How Are You ? <br>")
+// console.log("Dk : I am Fine What about You ? ");
+
+// document.write("MK : I am also good Bythaway What is your weekend Plan ? <br> ")
+// console.log("Dk : Nothing Just Chilling At the Home With Family");
+
+
+
+
+// ^here it will get display in the webpage means in the html body content it will go
+
+// document.write("Hello JS ! <br> ");
+// document.write("Lets Learn In the fun way ! <br>")
+// document.write("RCB Never Won the ILP Cup")
+
+
+// ^ Declaring the variables in the Javascript 
+// ! using 3 ways we can declare the variables in javascript 
+// ^(var , let , const)
+
+// ^ using var 
+
+var num = 10;
+console.log("num = " + num);
+
+var firstName = "Manoj";
+console.log("firstName : " + firstName);
+
+var isFollow = false;
+console.log("isFollow : " + isFollow);
+
+var character = 'a';
+console.log(character + 32);
+
+console.log(num);
+
+// ! doubt I not understood the type error ask it to the sir 
+
+
+
+
