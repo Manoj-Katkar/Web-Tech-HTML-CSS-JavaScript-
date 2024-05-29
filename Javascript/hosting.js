b338942d9da87b70508e3f25c8ce098c62c12c2d
Manoj-Katkar <education2001manojkatkar@gmail.com>
2024-05-29 18:55:21 +0530

    created the services section of the landscaper static website and learned the hosting types and temporal dead zone in javascript

---
+++ Javascript/hosting.js
@@ -0,0 +1,42 @@
+// varable hosting : all the declaration will goes to the top of that scope 
+// undefined is the datatype but not-define is the error most important point 
+
+// ! in the parsing stage only the hosting will happen 
+// console.log(a);
+// console.log(b);  //reference error 
+// console.log(c);   //reference error 
+
+var a = 20;
+let b = 50;
+const c = 3.14;
+
+// lets try now with the block 
+
+{
+    // ! FEW OF THE VARIABLE NAMES ARE THE RESERVED KEYWORD THAT THEY ARE WORKING ON THOSE NAMES HENCE USE THE DIFFERANT NAMES 
+    console.log(nam);
+
+    var nam = "Manoj";
+    let num1 = 20;
+    const PI = 3.14;
+
+    
+}
+
+
+function abc(){
+    var lastName = "Katkar";
+    let num2 = 65;
+    const ref = 89;
+}
+
+abc(); 
+
+
+
+// Function Hosting : Entire the function will move tothe top of the scope 
+
+
+
+
+
