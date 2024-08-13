130961f017528e24cf7d9354730686f4b8744ec0
Manoj-Katkar <education2001manojkatkar@gmail.com>
2024-08-13 13:05:31 +0530

    learned and created local storage implemented local storage for signup form and also validation for mobile number and email implemented

--- Dynamic Projects HTML CSS JS/signUp.js
+++ Dynamic Projects HTML CSS JS/signUp.js
@@ -8,6 +8,8 @@ let phoneNumber = document.querySelectorAll("input")[3];
 let createPassword = document.querySelectorAll("input")[4];
 let confirmPassword = document.querySelectorAll("input")[5];
 
+// firstName lastName  email  phoneNumber createPassword  confirmPassword
+
 // !Access the all the span tags 
 // !here e represent the error for all the input tags 
 let eFirst = document.querySelectorAll("span")[0];
@@ -34,6 +36,20 @@ console.log(ePhoneNumber);
 console.log(eCreatePass);
 console.log(eConfirmPass);
 
+// !creating the one empty array to store the each user data in the form of the array 
+// !I can store the multiple values in  the form of the object 
+
+let storage = [];
+
+
+let dataFromStorage = JSON.parse(localStorage.getItem("details"));
+
+
+//*here we are doing like appending in the array
+
+if(dataFromStorage){
+    storage = dataFromStorage;
+}
 
 
 
@@ -41,6 +57,28 @@ console.log(eConfirmPass);
 
 form.addEventListener("submit" , (event) => {
     // I have to restrict the default behaviour of the form 
+
+    //* cheking for the new mobile number that is entered by the user is already exist or not in the local storage array  memory 
+   
+    let mobileNumberCheck = storage.find((element) => {
+        if(element.phoneNum === phoneNumber.value){  //*cheking with already exist mobile with user typed mobile number
+            return element;
+        }
+    })
+
+    // *Now check that user is typing the same email id again 
+    let newEmailExistInStorage = storage.some((element) =>{
+        if(element.email ===  email.value){
+            return true;
+        }
+    })
+
+
+    // flag taken to add the data in the local storage
+    let flag = true;
+
+    
+     
 
 
     // !How to create the regular expression  (very Important)
@@ -60,6 +98,9 @@ form.addEventListener("submit" , (event) => {
 
     // ^and error should display in the UI 
         eFirst.innerHTML = "enter the first name";
+
+
+        flag = false;
         
     }
     else if(regx.test(firstName.value)){
@@ -72,6 +113,8 @@ form.addEventListener("submit" , (event) => {
         // means test given the false value in return 
         eFirst.innerHTML = "Invalid first name";
         event.preventDefault();
+
+        flag = false;
     }
 
 
@@ -80,12 +123,15 @@ form.addEventListener("submit" , (event) => {
 
 
     // !last Name validation
+
     if(lastName.value === ""){
         //^ then the form should not be submitted 
         event.preventDefault();
 
     // ^and error should display in the UI 
         eLast.innerHTML = "enter the last name";
+
+        flag = false;
         
     }
     else if(regx.test(lastName.value)){
@@ -98,6 +144,9 @@ form.addEventListener("submit" , (event) => {
         // means test given the false value in return 
         eLast.innerHTML = "Invalid last name";
         event.preventDefault();
+
+
+        flag = false;
     }
 
 
@@ -105,12 +154,20 @@ form.addEventListener("submit" , (event) => {
 
     // !Email validation
     // here we are not using the regular expression because already by default validation is done via html
+    if(newEmailExistInStorage){
+        eEmail.innerHTML = "Email Id Already Exist";
 
-    if(email.value === ""){
-        eEmail.innerHTML = "Enter the Email"
+        event.preventDefault();
+
+        flag = false;
+    }
+    else if(email.value === ""){
+        eEmail.innerHTML = "Enter the Email";
 
         //make sure that form should not get submitted 
         event.preventDefault();
+
+        flag = false;
     }
     else{
         eEmail.innerHTML = "";
@@ -124,13 +181,20 @@ form.addEventListener("submit" , (event) => {
 
     let mobileRegx = /^[6-9][0-9]{9}$/    //*[6-9 (it will take one number)] other rest nine numbers 
 
-    
+    if(mobileNumberCheck){
+        ePhoneNumber.innerHTML = "Mobile Number is Already exist";
+        event.preventDefault();
 
-    if(phoneNumber.value === ""){
+        flag = false;
+
+    }
+    else if(phoneNumber.value === ""){
         //it is error 
         ePhoneNumber.innerHTML = "Enter Phone Number";
 
         event.preventDefault();
+
+        flag = false;
 
     }
     else if(mobileRegx.test(phoneNumber.value)){
@@ -142,6 +206,9 @@ form.addEventListener("submit" , (event) => {
 
         // ^and I have prevent the default behaviour of  the event 
         event.preventDefault();
+
+
+        flag = false;
     }
 
 
@@ -157,6 +224,9 @@ form.addEventListener("submit" , (event) => {
     if(createPassword.value === ""){
         eCreatePass.innerHTML = "Enter the password";
         event.preventDefault();
+
+
+        flag = false;
     }
     else if(regPassword.test(createPassword.value)){
         // true means there is no error 
@@ -167,6 +237,9 @@ form.addEventListener("submit" , (event) => {
         eCreatePass.innerHTML = "Invalid Password";
 
         event.preventDefault();
+
+
+        flag = false;
     }
 
 
@@ -175,6 +248,9 @@ form.addEventListener("submit" , (event) => {
     if(confirmPassword.value === ""){
         eConfirmPass.innerHTML = "Enter the Correct the password";
         event.preventDefault();
+
+
+        flag = false;
     }
     else if(confirmPassword.value === createPassword.value){
         // means it is correct password entered by the user in the confirm password filed 
@@ -183,7 +259,56 @@ form.addEventListener("submit" , (event) => {
     else{
         eConfirmPass.innerHTML = "Entered password is not Matching please enter again";
         event.preventDefault();
+
+
+        flag = false;
     }
 
 
+
+
+
+
+// !Storing the data in the local storage 
+// firstName lastName  email  phoneNumber createPassword  confirmPassword
+// ^Approach to store the data in the local storage using t he flag means when the error is there from the user for the input value then it will not accept the wrong values 
+// ^but whenever the flag is true means no error from user site 
+
+if(flag){
+
+    let details = {
+        firstN : firstName.value,
+        lastN :lastName.value,
+        email :email.value,
+        phoneNum : phoneNumber.value,
+        pass : confirmPassword.value
+    }
+
+
+
+    // !adding the data in the local storage using the Array 
+    storage.push(details);
+
+    // adding the data in the local storage 
+    localStorage.setItem("details" , JSON.stringify(storage));
+
+    console.log(details);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
+
+// localStorage.clear();
