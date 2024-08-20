ab86c04caa7e619e5dd3dbc424e27c2f832526ff
Manoj-Katkar <education2001manojkatkar@gmail.com>
2024-08-20 13:02:43 +0530

    added the some functionality for the current question , answered question , visited but not answered questions

--- Dynamic Projects HTML CSS JS/Quize/quize.js
+++ Dynamic Projects HTML CSS JS/Quize/quize.js
@@ -1,7 +1,7 @@
 let storage = [
     {
         questionId : 1,
-        actualQuestion : "A person crosses a 600 m long street in 5 minutes. What is his speed in km per hour?",
+        actualQuestion : "1) A person crosses a 600 m long street in 5 minutes. What is his speed in km per hour?",
         options : ["3.6" ,  "7.2" ,  "8.4" , "10"],
         correctAns : "7.2",
         userAns : null,
@@ -10,7 +10,7 @@ let storage = [
 
     {
         questionId : 2,
-        actualQuestion : "An aeroplane covers a certain distance at a speed of 240 kmph in 5 hours. To cover the same distance in 1 hours, it must travel at a speed of:",
+        actualQuestion : "2) An aeroplane covers a certain distance at a speed of 240 kmph in 5 hours. To cover the same distance in 1 hours, it must travel at a speed of:",
         options : ["300 kmph" ,  "360 kmph"  , "600 kmph"  , "720 kmph"],
         correctAns : "720 kmph",
         userAns : null,
@@ -20,7 +20,7 @@ let storage = [
 
     {
         questionId : 3,
-        actualQuestion : "If a person walks at 14 km/hr instead of 10 km/hr, he would have walked 20 km more. The actual distance travelled by him is:",
+        actualQuestion : "3) If a person walks at 14 km/hr instead of 10 km/hr, he would have walked 20 km more. The actual distance travelled by him is:",
         options : [ "50 km" , "56 km" ,  "70 km" ,  "80 km"],
         correctAns : "50 km",
         userAns : null,
@@ -30,7 +30,7 @@ let storage = [
 
     {
         questionId : 4,
-        actualQuestion : "A train can travel 50% faster than a car. Both start from point A at the same time and reach point B 75 kms away from A at the same time. On the way, however, the train lost about 12.5 minutes while stopping at the stations. The speed of the car is:",
+        actualQuestion : "4) A train can travel 50% faster than a car. Both start from point A at the same time and reach point B 75 kms away from A at the same time. On the way, however, the train lost about 12.5 minutes while stopping at the stations. The speed of the car is:",
         options : ["100 kmph",  "110 kmph" , "120 kmph" , "130 kmph"],
         correctAns : "120 kmph",
         userAns : null,
@@ -40,7 +40,7 @@ let storage = [
 
     {
         questionId : 5,
-        actualQuestion : "Excluding stoppages, the speed of a bus is 54 kmph and including stoppages, it is 45 kmph. For how many minutes does the bus stop per hour?",
+        actualQuestion : "5) Excluding stoppages, the speed of a bus is 54 kmph and including stoppages, it is 45 kmph. For how many minutes does the bus stop per hour?",
         options : ["9" ,  "10"  , "12"  , "20"],
         correctAns : "10",
         userAns : null,
@@ -50,7 +50,7 @@ let storage = [
 
     {
         questionId : 6,
-        actualQuestion : `In a flight of 600 km, an aircraft was slowed down due to bad weather. Its average speed for the trip was reduced by 200 km/hr and the time of flight increased by 30 minutes. The duration of the flight is:`,
+        actualQuestion : `6) In a flight of 600 km, an aircraft was slowed down due to bad weather. Its average speed for the trip was reduced by 200 km/hr and the time of flight increased by 30 minutes. The duration of the flight is:`,
         options : ["1 hour" , "2 hours" ,"3 hours" ,"4 hours"],
         correctAns : "1 hour",
         userAns : null,
@@ -59,7 +59,7 @@ let storage = [
 
     {
         questionId : 7,
-        actualQuestion : "Excluding stoppages, the speed of a bus is 54 kmph and including stoppages, it is 45 kmph. For how many minutes does the bus stop per hour?",
+        actualQuestion : "7) Excluding stoppages, the speed of a bus is 54 kmph and including stoppages, it is 45 kmph. For how many minutes does the bus stop per hour?",
         options : ["9" ,  "10"  , "12"  , "20"],
         correctAns : "10",
         userAns : null,
@@ -111,11 +111,13 @@ function createBtn(){
 
 createBtn();
 
+let allBtn = actualButtonsContainer.querySelectorAll("button");
+
 // creating the function to display the question 
 // actualQuetionContainer
 
 function display(){
-    // make sure first it is cleared 
+    // make sure first previous questions options got cleared 
     actualOptionsContainer.innerHTML = "";
 
 
@@ -134,14 +136,28 @@ function display(){
 
         
 
-        //* every radio input button should be treated as the new for each option 
+        //* every radio input button should be treated as the new for each option and only one out of them should get selected hence giving the same name  
         opdRadio.name = "options";
 
         //* now above line computer understood only to select the one but for the user how to display for that below logic 
         let label = document.createElement("label");
         label.innerHTML = currentOption;
 
+        //! NOw I have to make sure that user selected the ans it should be permanet visible to the user 
+
+        if(storage[index].userAns === currentOption){  //*or I can compare with the opdRadio.value both are same 
+            opdRadio.checked = true;
+        }
+
         actualOptionsContainer.append(opdRadio , label);
+
+            //*then I have to change the color of the current question on where i am 
+            allBtn.forEach((currentBtn) => {
+                if(currentBtn.id - 1 === index){
+                    currentBtn.style.backgroundColor = "hotpink";
+                }
+         
+            })
 
     })
 }
@@ -149,8 +165,12 @@ function display(){
 display();
 
 
-// logic for the next button 
+//^ logic for the next button 
 Revisit_Next.addEventListener("click" , (event) => {
+    //*then first that question is visited change the color of that question button 
+
+    notSave();
+
     index = (index + 1) % storage.length;
     
     display();
@@ -160,6 +180,11 @@ Revisit_Next.addEventListener("click" , (event) => {
 
 // logic for the previous button 
 previous.addEventListener("click" , (event) => {
+
+    //*then first that question is visited change the color of that question button 
+    
+    notSave();
+
     index = (index - 1 + storage.length) % storage.length;
     
     display();
@@ -168,7 +193,108 @@ previous.addEventListener("click" , (event) => {
 // logic for Save_Next button 
 
 Save_Next.addEventListener("click" , (event) => {
+
+    //*then first that question is visited change the color of that question button 
+    
+    notSave();
+
+    saveAns();  //*calling the save ans  function because first i have to save and then go next 
+
     index = (index + 1) % storage.length;
     
     display();
 })
+
+// !Now I am linking the each actual button to its actual question 
+
+// let allBtn = actualButtonsContainer.querySelectorAll("button");  //*I have to declare this above before display 
+
+console.log(allBtn);
+
+// *iterating over the each button 
+
+function individualBtn(){
+    allBtn.forEach((currentBtn) => {
+        // console.log(currentBtn);
+        currentBtn.addEventListener("click" , (event) => {
+    
+
+
+            //*then first that question is visited change the color of that question button 
+            
+            notSave();
+            // *for each button I will add the event listner 
+            // we have to update the index also 
+            index = currentBtn.id - 1;
+            display();
+    
+        })
+        
+    })
+}
+
+
+individualBtn();
+
+
+// !Now we are going to save the answers 
+function saveAns(){
+    let options = document.querySelectorAll("input");
+
+    console.log(options);
+
+    // !now iterating over the options nodeList 
+
+    options.forEach((individualOption) => {
+        //*now when the user will check on the radio button input then that option should get saved 
+        if(individualOption.checked){
+            //*means the user have selected the one option out of the 4 
+            // console.log(individualOption); // here it is tag I have to get the value of that button 
+            console.log(individualOption.value);
+            
+            //*Now I have to update the userAns in storage Array 
+            storage[index].userAns = individualOption.value;
+
+            console.log(storage[index].userAns);
+            console.log(storage);
+            
+
+            // //* Now I have to also update visited to true 
+            // storage[index].visited = true;
+
+            //*then I have to change the color of that question to green also 
+            allBtn.forEach((currentBtn) => {
+                if(currentBtn.id - 1 === index){
+                    currentBtn.style.backgroundColor = "green";
+                }
+         
+            })
+            
+        }
+    })
+    
+    
+}
+
+
+// !function to not save the options for particular question
+
+function notSave(){
+
+    //* Now I have to also update visited to true 
+    storage[index].visited = true;
+
+    if(!storage[index].userAns){
+        // menas the user ans is not given 
+
+            //*then I have to change the color of that question to purple means that question is already visited also 
+            allBtn.forEach((currentBtn) => {
+                if(currentBtn.id - 1 === index){
+                    currentBtn.style.backgroundColor = "purple";
+                }
+         
+            })
+    }
+}
+
+
